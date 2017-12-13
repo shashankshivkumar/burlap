f1737b2b77fbd3a46cb697165e84290d586dad14
jorgeamendezm <jorgeamendezm@gmail.com>
2017-12-13 18:52:22 -0500

    Continuing basic GridWorld tests on MLIRL

--- src/test/java/burlap/testing/MyFirstTest.java
+++ src/test/java/burlap/testing/MyFirstTest.java
@@ -6,10 +6,13 @@ import burlap.behavior.singleagent.auxiliary.EpisodeSequenceVisualizer;
 import burlap.behavior.singleagent.learnfromdemo.mlirl.MLIRL;
 import burlap.behavior.singleagent.learnfromdemo.mlirl.MLIRLRequest;
 import burlap.behavior.singleagent.learnfromdemo.mlirl.commonrfs.LinearStateDifferentiableRF;
+import burlap.behavior.singleagent.learnfromdemo.mlirl.support.DifferentiableRF;
 import burlap.behavior.singleagent.learning.tdmethods.QLearning;
+import burlap.debugtools.DPrint;
 import burlap.domain.singleagent.gridworld.GridWorldDomain;
 import burlap.domain.singleagent.gridworld.GridWorldTerminalFunction;
 import burlap.domain.singleagent.gridworld.GridWorldVisualizer;
+import burlap.domain.singleagent.gridworld.state.GridLocation;
 import burlap.domain.singleagent.gridworld.state.GridWorldState;
 import burlap.statehashing.simple.SimpleHashableStateFactory;
 
@@ -29,29 +32,63 @@ public class MyFirstTest {
     	gwd.setMapToFourRooms();
     	gwd.setTf(new GridWorldTerminalFunction(10, 10));
     	OOSADomain domain = gwd.generateDomain();
-    	Environment env = new SimulatedEnvironment(domain, new GridWorldState(0, 0));
+    	GridLocation gridloc = new GridLocation(10,10,"loc0");
+    	Environment env = new SimulatedEnvironment(domain, new GridWorldState(0, 0, gridloc));
 
     	//create a Q-learning agent
     	QLearning agent = new QLearning(domain, 0.99, new SimpleHashableStateFactory(), 1.0, 1.0);
 
     	//run 100 learning episode and save the episode results
     	List<Episode> episodes = new ArrayList<Episode>();
-    	for(int i = 0; i < 100; i++){
+    	for(int i = 0; i < 200; i++){
     		episodes.add(agent.runLearningEpisode(env));
     		env.resetEnvironment();
     	}
-
+    	
+    	List<Episode> goodEpisodes = new ArrayList<Episode>(episodes.subList(99, 200));
     	//visualize the completed learning episodes
     	//new EpisodeSequenceVisualizer(GridWorldVisualizer.getVisualizer(gwd.getMap()), domain, episodes);
     	
     	PFFeatures featureGen = new PFFeatures(domain);
-    	LinearStateDifferentiableRF rf = new LinearStateDifferentiableRF(featureGen, 6);
+    	LinearStateDifferentiableRF rf = new LinearStateDifferentiableRF(featureGen, 5);
     	SimpleHashableStateFactory hashingFactory = new SimpleHashableStateFactory();
-    	MLIRLRequest request = new MLIRLRequest(domain, episodes, rf, hashingFactory);
+    	MLIRLRequest request = new MLIRLRequest(domain, goodEpisodes, rf, hashingFactory);
     	
-    	MLIRL mlirl = new MLIRL(request, .0003, .0001, 100);
-    	
+    	MLIRL mlirl = new MLIRL(request, .00003, .01, 500);
+    	    	
     	mlirl.performIRL();
+    	
+    	DifferentiableRF rf_new = request.getRf();
+//    	LinearStateDifferentiableRF rf_new = new LinearStateDifferentiableRF(featureGen, 5);
+//    	rf_new.setParameter(0, 0);
+//    	rf_new.setParameter(1,0);
+//    	rf_new.setParameter(2,0);
+//    	rf_new.setParameter(3,5);
+//    	rf_new.setParameter(4,0);
+    	
+    	
+    	// test learned reward
+    	GridWorldDomain gwd_learned = new GridWorldDomain(11, 11);
+    	gwd_learned.setMapToFourRooms();
+    	gwd_learned.setTf(new GridWorldTerminalFunction(10, 10));
+    	gwd_learned.setRf(rf_new);
+    	OOSADomain domain_learned = gwd_learned.generateDomain();
+    	Environment env_learned = new SimulatedEnvironment(domain_learned, new GridWorldState(0, 0, gridloc));
+
+    	//create a Q-learning agent
+    	QLearning agent_learned = new QLearning(domain_learned, 0.99, new SimpleHashableStateFactory(), 1.0, 1.0);
+
+    	//run 100 learning episode and save the episode results
+    	List<Episode> episodes_learned = new ArrayList<Episode>();
+    	for(int i = 0; i < 200; i++){
+    		episodes_learned.add(agent_learned.runLearningEpisode(env_learned));
+    		env_learned.resetEnvironment();
+    	}
+    	
+    	//visualize the completed learning episodes
+    	new EpisodeSequenceVisualizer(GridWorldVisualizer.getVisualizer(gwd_learned.getMap()), domain_learned, episodes_learned);
+
+    	
     	    	
 	}
 }
